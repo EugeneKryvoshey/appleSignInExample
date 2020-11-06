9573d519526f7759a62faaed3a31161d8dbfbb3a
Eugene Kryvoshey <--add>
2020-11-06 17:01:30 +0200

    Added Firebase callbacks. Changed style of Activity and button.

--- app/src/main/java/com/ekryvoshey/applesigninexample/MainActivity.kt
+++ app/src/main/java/com/ekryvoshey/applesigninexample/MainActivity.kt
@@ -3,6 +3,11 @@ package com.ekryvoshey.applesigninexample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ekryvoshey.applesigninexample.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.OAuthProvider
+import com.google.firebase.auth.zze
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +16,44 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            signInWithApple()
+        }
+    }
+
+    private fun signInWithApple() {
+        val pending = FirebaseAuth.getInstance().pendingAuthResult
+        if (pending != null) {
+            pending
+                .addOnSuccessListener { onSuccess(it) }
+                .addOnFailureListener { onFailure(it) }
+        } else {
+            val provider = OAuthProvider.newBuilder("apple.com").apply {
+                scopes = listOf("email", "name")
+                addCustomParameter("locale", "uk")
+            }.build()
+            FirebaseAuth.getInstance().startActivityForSignInWithProvider(this, provider)
+                .addOnSuccessListener { onSuccess(it) }
+                .addOnFailureListener { onFailure(it) }
+        }
+    }
+
+    private fun onSuccess(authResult: AuthResult) {
+        val token = (authResult.credential as zze).idToken
+        Snackbar.make(
+            binding.root,
+            "Success to login with Apple ID",
+            2
+        ).show()
+    }
+
+    private fun onFailure(exception: Exception) {
+        val errorText = exception.message ?: "Error unknown"
+        Snackbar.make(
+            binding.root,
+            "Failed to login with Apple ID: $errorText",
+            5
+        ).show()
     }
 }
